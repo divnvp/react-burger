9186b5d3576b93606f8e8166221fd033c7476e33
Diana Pimenova <dpimenova@diasoft.ru>
2024-10-16 16:59:52 +0300

    Сверстала страницу восстановления пароля

--- src/pages/forgot-password/forgot-password.tsx
+++ src/pages/forgot-password/forgot-password.tsx
@@ -1,3 +1,49 @@
+import { Layout } from '../../components/layout/layout';
+import forgotPasswordStyles from './forgot-password.module.css';
+import {
+  Button,
+  Input
+} from '@ya.praktikum/react-developer-burger-ui-components';
+import React from 'react';
+import registerStyles from '../register/register.module.css';
+import { Link } from 'react-router-dom';
+
 export function ForgotPasswordPage() {
-  return <></>;
+  const [email, setEmail] = React.useState('');
+
+  return (
+    <Layout>
+      <div className={forgotPasswordStyles.grid}>
+        <p className='text text_type_main-medium pb-6'>Восстановление пароля</p>
+
+        <div className='pb-6'>
+          <Input
+            type='text'
+            placeholder='Укажите e-mail'
+            onChange={e => setEmail(e.target.value)}
+            value={email}
+            name={'name'}
+            error={false}
+            errorText={'Ошибка'}
+            size={'default'}
+            extraClass='ml-1'
+            onPointerEnterCapture={undefined}
+            onPointerLeaveCapture={undefined}
+          />
+        </div>
+        <Button htmlType='button' type='primary' size='medium'>
+          Восстановить
+        </Button>
+
+        <div className={`${registerStyles.textRow} pb-4 pt-20`}>
+          <p className='text text_type_main-default text_color_inactive'>
+            Вспомнили пароль?
+          </p>
+          <Link to='/login' className='text text_type_main-default'>
+            <p>Войти</p>
+          </Link>
+        </div>
+      </div>
+    </Layout>
+  );
 }
