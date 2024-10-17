c1448cda1b000afab11129df146ce6773a68c923
Diana Pimenova <dpimenova@diasoft.ru>
2024-10-17 17:59:03 +0300

    Добавила методы для профиля и утилиту получения куки

---
+++ src/shared/api/user.service.ts
@@ -0,0 +1,36 @@
+import { request } from '../utils/request';
+import { Request } from '../consts/request.enum';
+import { getCookie } from '../utils/get-cookie';
+
+const getUser = () => {
+  return request(`/auth/user`, {
+    method: Request.GET,
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ' + getCookie('token')
+    },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer'
+  });
+};
+
+const updateUser = (credits: any) => {
+  return request(`/auth/user`, {
+    method: Request.PATCH,
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ' + getCookie('token')
+    },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+    body: JSON.stringify(credits)
+  });
+};
+
+export { getUser };
