a036e2c148e0aa3605177b16aac3d3d10ed2a8fc
Diana Pimenova <dpimenova@diasoft.ru>
2024-11-21 13:04:32 +0300

    Изменила тип AppActions

--- src/services/middleware/socket-middleware.ts
+++ src/services/middleware/socket-middleware.ts
@@ -15,37 +15,37 @@ export const socketMiddleware = (
       const { wsInit, wsSendMessage, onOpen, onClose, onError, onMessage } =
         wsActions;
       const { user } = (getState() as { user: any }).user;
-      if (type === wsInit && user) {
-        socket = new WebSocket(`${wsUrl}?token=${user.token}`);
-      }
-      if (socket) {
-        socket.onopen = event => {
-          dispatch({ type: onOpen, payload: event });
-        };
-
-        socket.onerror = event => {
-          dispatch({ type: onError, payload: event });
-        };
-
-        socket.onmessage = event => {
-          const { data } = event;
-          const parsedData = JSON.parse(data);
-          const { success, ...restParsedData } = parsedData;
-
-          dispatch({
-            type: onMessage,
-            payload: { ...restParsedData }
-          });
-        };
-
-        socket.onclose = event => {
-          dispatch({ type: onClose, payload: event });
-        };
-
-        if (type === wsSendMessage) {
-          const payload = action.payload;
-        }
-      }
+      // if (type === wsInit && user) {
+      //   socket = new WebSocket(`${wsUrl}?token=${user.token}`);
+      // }
+      // if (socket) {
+      //   socket.onopen = event => {
+      //     dispatch({ type: onOpen, payload: event });
+      //   };
+      //
+      //   socket.onerror = event => {
+      //     dispatch({ type: onError, payload: event });
+      //   };
+      //
+      //   socket.onmessage = event => {
+      //     const { data } = event;
+      //     const parsedData = JSON.parse(data);
+      //     const { success, ...restParsedData } = parsedData;
+      //
+      //     dispatch({
+      //       type: onMessage,
+      //       payload: { ...restParsedData }
+      //     });
+      //   };
+      //
+      //   socket.onclose = event => {
+      //     dispatch({ type: onClose, payload: event });
+      //   };
+      //
+      //   if (type === wsSendMessage) {
+      //     const payload = action.payload;
+      //   }
+      // }
 
       next(action);
     };
--- src/services/types/index.ts
+++ src/services/types/index.ts
@@ -1,8 +1,21 @@
 import store from '../store';
 import { ThunkAction, ThunkDispatch } from 'redux-thunk';
-import { AnyAction } from 'redux';
+import { TBurgerIngredientsActions } from '../actions/burger-ingredients';
+import { TBurgerConstructorActions } from '../actions/burger-constructor';
+import { TForgotPasswordActions } from '../actions/forgot-password';
+import { TLoginActions } from '../actions/login';
+import { TRegistrationActions } from '../actions/registration';
+import { TResetPasswordAction } from '../actions/reset-password';
+import { TUserActions } from '../actions/user';
 
-export type AppActions = AnyAction;
+export type AppActions =
+  | TBurgerIngredientsActions
+  | TBurgerConstructorActions
+  | TForgotPasswordActions
+  | TLoginActions
+  | TRegistrationActions
+  | TResetPasswordAction
+  | TUserActions;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = ThunkDispatch<RootState, unknown, AppActions>;
 export type AppThunkAction<ReturnType = void> = ThunkAction<
