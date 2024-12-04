9874d5df1b89a363d2664d9927802b34b853e525
Diana Pimenova <dpimenova@diasoft.ru>
2024-12-04 12:53:44 +0300

    Покрыла тестами редюсер Feeds

--- src/services/reducers/feeds.test.ts
+++ src/services/reducers/feeds.test.ts
@@ -1,25 +1,56 @@
 import { feedsReducer } from './feeds';
-import { getFeedsActions, TFeedActions } from '../actions/feeds';
-import { Feed, FeedDetail } from '../../shared/models/feed.type';
-import { Status } from '../../shared/consts/status.enum';
-import { v4 } from 'uuid';
+import {
+  getFeedRejected,
+  getFeedsActions,
+  getFeedsRequest,
+  TFeedActions
+} from '../actions/feeds';
+import { Feed } from '../../shared/models/feed.type';
 
-const _id = v4();
-
-const orderTest: FeedDetail = {
-  _id,
-  status: Status.Done,
-  number: 'string;',
-  name: 'string;',
-  ingredients: [],
-  createdAt: new Date(),
-  updatedAt: new Date()
-};
 const feedTest: Feed = {
-  orders: [orderTest],
   success: true,
-  total: 2,
-  totalToday: 1
+  orders: [
+    {
+      _id: '674fe769e367de001daf69af',
+      ingredients: [
+        '643d69a5c3f7b9001cfa093e',
+        '643d69a5c3f7b9001cfa093d',
+        '643d69a5c3f7b9001cfa093d'
+      ],
+      status: 'done',
+      name: 'Флюоресцентный люминесцентный бургер',
+      createdAt: '2024-12-04T05:23:53.907Z',
+      updatedAt: '2024-12-04T05:23:54.886Z',
+      number: 61505
+    },
+    {
+      _id: '674f8d5de367de001daf698a',
+      ingredients: [
+        '643d69a5c3f7b9001cfa093d',
+        '643d69a5c3f7b9001cfa093e',
+        '643d69a5c3f7b9001cfa093e',
+        '643d69a5c3f7b9001cfa0940',
+        '643d69a5c3f7b9001cfa093e',
+        '643d69a5c3f7b9001cfa0940'
+      ],
+      status: 'done',
+      name: 'Флюоресцентный люминесцентный метеоритный бургер',
+      createdAt: '2024-12-03T22:59:41.488Z',
+      updatedAt: '2024-12-03T22:59:42.482Z',
+      number: 61504
+    },
+    {
+      _id: '674f71c8e367de001daf694f',
+      ingredients: ['643d69a5c3f7b9001cfa093d', '643d69a5c3f7b9001cfa093e'],
+      status: 'done',
+      name: 'Флюоресцентный люминесцентный бургер',
+      createdAt: '2024-12-03T21:02:00.718Z',
+      updatedAt: '2024-12-03T21:02:01.600Z',
+      number: 61493
+    }
+  ],
+  total: 61139,
+  totalToday: 149
 };
 const initialState = {
   feeds: undefined,
@@ -34,9 +65,30 @@ describe('Feeds Reducer', () => {
   });
 
   it('should get feeds', () => {
-    expect(feedsReducer(initialState, getFeedsActions(feedTest))).toEqual({
+    expect(
+      feedsReducer(
+        initialState,
+        getFeedsActions(JSON.stringify({ feed: feedTest }) as unknown as Feed)
+      )
+    ).toEqual({
       ...initialState,
-      feeds: [feedTest]
+      feeds: { feed: feedTest }
+    });
+  });
+
+  it('should catch ingredients error', () => {
+    expect(
+      feedsReducer(initialState, getFeedRejected({ error: 'lalala' }))
+    ).toEqual({
+      ...initialState,
+      error: { error: 'lalala' }
+    });
+  });
+
+  it('should ingredients request', () => {
+    expect(feedsReducer(initialState, getFeedsRequest())).toEqual({
+      ...initialState,
+      error: null
     });
   });
 });
