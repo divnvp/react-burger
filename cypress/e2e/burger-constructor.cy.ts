863d2ae63cc591f5577d3627fe9ff876b1598a2c
Diana Pimenova <dpimenova@diasoft.ru>
2024-12-08 14:06:28 +0300

    Добавила тестирование p-тега на основе Cypress

--- cypress/e2e/burger-constructor.cy.ts
+++ cypress/e2e/burger-constructor.cy.ts
@@ -1,5 +1,9 @@
-describe('service is available', function () {
-  it('should be available on localhost:3000', function () {
+describe('service is available', () => {
+  it('should be available on localhost:3000', () => {
     cy.visit('http://localhost:3000');
+    cy.get('p.text.text_type_main-large.pb-5').should(
+      'have.text',
+      'Соберите бургер'
+    );
   });
 });
