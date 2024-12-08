e7038907dd180594e30799fac5037d6de12f7973
Diana Pimenova <dpimenova@diasoft.ru>
2024-12-08 14:26:37 +0300

    Поправила логику теста

--- cypress/e2e/burger-constructor.cy.ts
+++ cypress/e2e/burger-constructor.cy.ts
@@ -1,9 +1,19 @@
 describe('service is available', () => {
-  it('should be available on localhost:3000', () => {
+  beforeEach(function () {
     cy.visit('http://localhost:3000');
+  });
+
+  it('initial state of page', () => {
     cy.get('p.text.text_type_main-large.pb-5').should(
       'have.text',
       'Соберите бургер'
     );
+    cy.get('button').contains('Оформить заказ').should('be.disabled');
   });
+
+  // it('should open delivery page after formed button click', function () {
+  //   cy.get('button').contains('Оформить заказ');
+  //   // .contains('Продолжить оформление').click();
+  //   // cy.contains('Доставка');
+  // });
 });
