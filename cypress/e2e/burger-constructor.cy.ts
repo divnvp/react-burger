dd4aa9305bbe2f1332394117ba76a77c60c26ec9
Diana Pimenova <dpimenova@diasoft.ru>
2024-12-09 14:18:38 +0300

    Добавила тесты для оформления заказа авторизованным пользователем

--- cypress/e2e/burger-constructor.cy.ts
+++ cypress/e2e/burger-constructor.cy.ts
@@ -51,4 +51,34 @@ describe('Burger Constructor works correctly', () => {
       cy.get('button').contains('Оформить заказ').click();
     }
   });
+
+  it('should authorization work', () => {
+    cy.visit('http://localhost:3000/login');
+    cy.get('input.text.input__textfield.text_type_main-default')
+      .first()
+      .type('dxs@d.ru');
+    cy.get('input.text.input__textfield.text_type_main-default')
+      .last()
+      .type('12345678');
+    cy.get('button').click();
+
+    const dataTransfer = new DataTransfer();
+
+    if (
+      cy
+        .get('p.text.text_type_main-medium')
+        .last()
+        .should('have.text', 'Начинки')
+    ) {
+      cy.get('div.burger-ingredients_card__AqzE1')
+        .first()
+        .trigger('dragstart', { dataTransfer });
+      cy.get('p.text.text_type_main-default.text_color_inactive')
+        .contains(
+          'Пожалуйста, перенесите сюда булку и ингредиенты для создания заказа'
+        )
+        .trigger('drop', { dataTransfer });
+      cy.get('button').contains('Оформить заказ').click();
+    }
+  });
 });
